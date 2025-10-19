a4142bc7bc78f0480cf3d1cb94d905b542254743
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2025-10-19 13:08:32 -0300

    feat: remame App component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,14 @@
 import { Header } from '../../components/Header'
 
-function App() {
+import './styles.css'
+
+function Home() {
   return (
-    <div className="App">
+    <div className="home">
       <Header />
+      <div className="content"></div>
     </div>
   );
 }
 
-export default App;
+export default Home;
